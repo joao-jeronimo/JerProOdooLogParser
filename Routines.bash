b0969bf374a342dcf67ac24d376ae52e057ccfec
João Jerónimo <joao.jeronimo.pro@gmail.com>
2025-11-12 17:41:26 +0000

    Permitir chamar o pytest.

---
+++ Routines.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e -x
+
+PYTEST_FLAGS="-o log_cli=true -o log_cli_level=INFO"
+
+case $1 in
+  deps_devel)
+    pushd JerProOdooLogParser/
+    python3 -m pip install --upgrade -r requirements.txt
+    python3 -m pip uninstall pytest-xdist pytest-parallel
+    popd
+    ;;
+  
+  test_unit)
+    pushd JerProOdooLogParser/
+    pytest $PYTEST_FLAGS -m unit
+    popd
+    ;;
+  *)
+    echo "Unknown routine name: $1"
+    exit -1
+    ;;
+esac
