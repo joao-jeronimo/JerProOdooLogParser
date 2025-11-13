2fb58b924651976b0b67e825a16f7fa54c0f388f
João Jerónimo <joao.jeronimo.pro@gmail.com>
2025-11-13 11:17:00 +0000

    Faltava instalar o pytest.

--- Routines.bash
+++ Routines.bash
@@ -6,6 +6,7 @@ PYTEST_FLAGS="-o log_cli=true -o log_cli_level=INFO"
 case $1 in
   deps_devel)
     pushd JerProOdooLogParser/
+    sudo apt-get install 
     python3 -m pip install --upgrade -r requirements.txt
     python3 -m pip uninstall pytest-xdist pytest-parallel
     popd
