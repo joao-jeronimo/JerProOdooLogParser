fd504e75a80843adefd790a3ae9f40a8ecdf826e
João Jerónimo <joao.jeronimo.pro@gmail.com>
2025-11-13 18:27:54 +0000

    The --always-succeed flag: Tested and Implemented.

--- JerProOdooLogParser/OdooLogParser.py
+++ JerProOdooLogParser/OdooLogParser.py
@@ -13,21 +13,26 @@ def Main(exec_name, exec_argv):
     parser = argparse.ArgumentParser(description='A program for parsing and resuming Odoo logs.')
     parser.add_argument('--odoolog', type=str,
         help=('Odoo ligfile path.'))
+    parser.add_argument('--always-succeed', action='store_const', const=True, default=False,
+        help=('Always return SUCCESS regardles of test failures.'))
     args = parser.parse_args(args=exec_argv)
     ## Resume the logfile:
     with open(args.odoolog, "r") as logfilo:
         logparser = odoo_log_parser.OdooTestDigest(logfilo)
         digest = logparser.get_full_test_digest()
-    ### See if there are only sucesses:
-    all_success = True
-    for dbreport in digest.values():
-        if len(dbreport['tests_failing']) > 0:
-            all_success = False
-        if len(dbreport['tests_errors']) > 0:
-            all_success = False
-        if len(dbreport['setup_errors']) > 0:
-            all_success = False
-    ### Devise a proper return value in POSIX language:
-    return 0 if all_success else 1
+    ### See if there are only sucesses, unless otherwise requested:
+    if args.always_succeed:
+        return 0
+    else:
+        all_success = True
+        for dbreport in digest.values():
+            if len(dbreport['tests_failing']) > 0:
+                all_success = False
+            if len(dbreport['tests_errors']) > 0:
+                all_success = False
+            if len(dbreport['setup_errors']) > 0:
+                all_success = False
+        ### Devise a proper return value in POSIX language:
+        return 0 if all_success else 1
 
 if __name__ == "__main__": exit(Main(exec_name=sys.argv[0], exec_argv=sys.argv[1:]))
--- JerProOdooLogParser/odoo_log_parser/testcases/test_Frontend_OdooLogParser.py
+++ JerProOdooLogParser/odoo_log_parser/testcases/test_Frontend_OdooLogParser.py
@@ -12,8 +12,8 @@ class TestFrontend_OdooLogParser(unittest.TestCase, assert_mixins.ElementaryMixi
     
     def test_Main_fails_on_nonsucess_logfile(self):
         """
-        Method Main() must return non-zero if the logfile
-        contains traces non-successfull test.
+        Method Main() must return non-zero if the logfile contains traces
+        non-successfull test.
         """
         self.assertPosixFailure(frontend_OdooLogParser.Main("OdooLogParser.py", ['--odoolog', os.path.join(FIXTURES_DIR, 'odoolog_errors_in_called_by_setup.log')]))
         self.assertPosixFailure(frontend_OdooLogParser.Main("OdooLogParser.py", ['--odoolog', os.path.join(FIXTURES_DIR, 'odoolog_skels_demo.log')]) )
@@ -27,6 +27,18 @@ class TestFrontend_OdooLogParser(unittest.TestCase, assert_mixins.ElementaryMixi
         self.assertPosixSuccess(frontend_OdooLogParser.Main("OdooLogParser.py", ['--odoolog', os.path.join(FIXTURES_DIR, 'odoolog_mod_install_errors.log')]) )
         # Module version string errors are not detected as such for now:
         self.assertPosixSuccess(frontend_OdooLogParser.Main("OdooLogParser.py", ['--odoolog', os.path.join(FIXTURES_DIR, 'odoolog_wrong_version_string.log')]) )
+    
+    def test_Main_always_succeed_avoids_failing(self):
+        """
+        Method Main() must have a --always-succeed flag that causes it to never
+        return a failure code.
+        """
+        filenames = [
+            'odoolog_errors_in_called_by_setup.log',    'odoolog_skels_demo.log',       'odoolog_two_succeeded.log',        'odoolog_with_setup_errors.log',
+            'odoolog_with_test_errors.log',             'odoolog_moddep_errors.log',    'odoolog_mod_install_errors.log',   'odoolog_wrong_version_string.log',
+            ]
+        for fn in filenames:
+            self.assertPosixSuccess(frontend_OdooLogParser.Main("OdooLogParser.py", ['--always-succeed', '--odoolog', os.path.join(FIXTURES_DIR, fn)]))
     
     ############################################################
     ############################################################
