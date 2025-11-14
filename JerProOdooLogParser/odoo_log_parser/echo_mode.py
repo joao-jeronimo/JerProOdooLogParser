787998a2b0a7c40ce7c5488c95bcf70fed2061bb
João Jerónimo <joao.jeronimo.pro@gmail.com>
2025-11-14 12:39:37 +0000

    Mode to print the python digest as is, as a string: Tested and Implemented.

--- JerProOdooLogParser/odoo_log_parser/echo_mode.py
+++ JerProOdooLogParser/odoo_log_parser/echo_mode.py
@@ -3,4 +3,12 @@
 class EchoMode:
     @classmethod
     def get_echo_plugin(self, mode):
-        return None
+        """
+        Returna a function to convert a log digest into a string
+        to be printed-out to the user.
+            mode    The name of a mode.
+        """
+        if mode == 'python':
+            return repr
+        else:
+            return None
--- JerProOdooLogParser/odoo_log_parser/testcases/test_echo_mode.py
+++ JerProOdooLogParser/odoo_log_parser/testcases/test_echo_mode.py
@@ -14,3 +14,10 @@ class TestEchoMode(unittest.TestCase):
         Method get_echo_plugin() must return None on invalid echo modes.
         """
         self.assertIsNone( odoo_log_parser.EchoMode.get_echo_plugin("this-is-an-invalid-echo-mode-name") )
+    
+    def test_get_echo_plugin_python(self):
+        """
+        Method get_echo_plugin() when called with 'python' must return the repr() function.
+        """
+        the_funk = odoo_log_parser.EchoMode.get_echo_plugin("python")
+        self.assertEqual( the_funk('thing'), "'thing'" )
