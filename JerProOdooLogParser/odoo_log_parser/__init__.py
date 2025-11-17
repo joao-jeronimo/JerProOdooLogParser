3066bd44bfc1db4f27fe55923b7c150884c26f7e
João Jerónimo <joao.jeronimo.pro@gmail.com>
2025-11-17 15:27:34 +0000

    The package was not being imported.

--- JerProOdooLogParser/odoo_log_parser/__init__.py
+++ JerProOdooLogParser/odoo_log_parser/__init__.py
@@ -1,6 +1,7 @@
 from .python_log_parser import *
 from .odoo_test_digest import *
 from .echo_mode import *
+from .echo_modes import *
 
 __version__ = "2.0.1"
 __release_notes_2_0_1__ = "Using special vars for determining the test fixtures paths."
